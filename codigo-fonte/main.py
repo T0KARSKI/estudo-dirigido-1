ba4948ae0b4d3c408b0f97e60adfddb0dbccb427
T0KARSKI <gabriel.tokarski.almeida@gmail.com>
2026-04-13 00:13:13 -0300

    Add main.py to demonstrate search algorithms

---
+++ codigo-fonte/main.py
@@ -0,0 +1,18 @@
+from bfs import bfs
+from dfs import dfs
+from astar import astar
+from greedy import greedy
+from hill_climbing import hill_climbing
+
+# Exemplo simples de estado inicial (3x3)
+initial_state = [
+    [0, 1, 0],
+    [1, 0, 1],
+    [0, 1, 0]
+]
+
+print("BFS:", bfs(initial_state))
+print("DFS:", dfs(initial_state))
+print("A*:", astar(initial_state))
+print("Greedy:", greedy(initial_state))
+print("Hill Climbing:", hill_climbing(initial_state))
